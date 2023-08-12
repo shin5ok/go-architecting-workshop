7c822c6a554d1bd89b07b60dca950c724ae3b20e
shin5ok <shin5ok@55mp.com>
2023-08-13 01:53:50 +0900

    tidy up

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,17 +1,5 @@
 /*
-Copyright 2023 Google LLC
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Work in progress
 */
 package main
 
@@ -29,10 +17,8 @@ import (
 )
 
 var (
-	appName       = "myapp"
 	spannerString = os.Getenv("SPANNER_STRING")
 	redisHost     = os.Getenv("REDIS_HOST")
-	projectId     = os.Getenv("GOOGLE_CLOUD_PROJECT")
 )
 
 type Serving struct {
@@ -70,7 +56,13 @@ func main() {
 	}
 
 	/* Just to test */
-	userId, _ := uuid.NewRandom()
+	userId, err := uuid.NewRandom()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(os.Args) != 2 {
+		log.Fatal("error: invalid number of args")
+	}
 	userName := os.Args[1]
 	userParams := game.UserParams{UserID: userId.String(), UserName: userName}
 	err = s.Client.CreateUser(ctx, os.Stdout, userParams)
