d4df8fe29d378b4123108654785109de090e5fdc
shin5ok <shin5ok@55mp.com>
2023-08-27 11:22:46 +0900

    move no main funcs to internal

--- cmd/api/internal/otel.go
+++ cmd/api/internal/otel.go
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package main
+package internal
 
 import (
 	"go.opentelemetry.io/otel"
@@ -29,7 +29,7 @@ import (
 	gcppropagator "github.com/GoogleCloudPlatform/opentelemetry-operations-go/propagator"
 )
 
-func newTracer(projectId string) (*sdktrace.TracerProvider, error) {
+func NewTracer(projectId string) (*sdktrace.TracerProvider, error) {
 
 	exporter, err := texporter.New(texporter.WithProjectID(projectId))
 	if err != nil {
