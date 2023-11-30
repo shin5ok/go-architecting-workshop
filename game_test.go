c71b20aacd4d3b9a23cf2a6936dd823222aaa3ba
shin5ok <shin5ok@55mp.com>
2023-12-01 01:45:22 +0900

    prepare a test

---
+++ game_test.go
@@ -0,0 +1,114 @@
+package game
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/go-redis/redis"
+	"github.com/shin5ok/go-architecting-workshop/testutil"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	fakeDbString = os.Getenv("SPANNER_STRING") + testutil.GenStr()
+	fakeServing  Serving
+
+	noCleanup = os.Getenv("NO_CLEANUP") != ""
+
+	itemTestID = "d169f397-ba3f-413b-bc3c-a465576ef06e"
+	userTestID string
+)
+
+type Serving struct {
+	Client dbClient
+}
+
+func TestNewClient(t *testing.T) {
+	ctx := context.Background()
+
+	client, err := NewClient(ctx, fakeDbString, nil)
+	assert.IsType(t, client, dbClient{})
+
+	if err != nil {
+		t.Fatalf("NewClient: %v", err)
+	}
+}
+
+type dummyCaching struct{}
+
+func (c *dummyCaching) Get(key string) (string, error) {
+	return "", nil
+}
+
+func (c *dummyCaching) Set(key string, data string) error {
+	return nil
+}
+
+func init() {
+
+	log.Println("Creating " + fakeDbString)
+
+	if match, _ := regexp.MatchString("^projects/your-project-id/", fakeDbString); match {
+		os.Setenv("SPANNER_EMULATOR_HOST", "localhost:9010")
+	}
+
+	ctx := context.Background()
+
+	var c Cacher
+
+	// no use redis
+	c = &dummyCaching{}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:        "127.0.0.1:6379",
+		Password:    "",
+		DB:          0,
+		PoolSize:    10,
+		PoolTimeout: 30 * time.Second,
+		DialTimeout: 1 * time.Second,
+	})
+
+	c = &Caching{RedisClient: rdb}
+
+	fmt.Printf("cache %#+v\n", c)
+
+	schemaFiles, err := filepath.Glob("schemas/*_ddl.sql")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := testutil.InitData(ctx, fakeDbString, schemaFiles); err != nil {
+		log.Fatal(err)
+	}
+
+	dmlFiles, err := filepath.Glob("schemas/*_dml.sql")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := testutil.MakeData(ctx, fakeDbString, dmlFiles); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Test_cleaning(t *testing.T) {
+	t.Cleanup(
+		func() {
+			if noCleanup {
+				t.Log("###########", "skip cleanup")
+				return
+			}
+			ctx := context.Background()
+			if err := testutil.DropData(ctx, fakeDbString); err != nil {
+				t.Error(err)
+			}
+			t.Log("cleanup test data")
+		},
+	)
+}
